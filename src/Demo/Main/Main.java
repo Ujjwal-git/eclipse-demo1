6bb6bb6d0821ab93031ac78f26a52e6d24631cfb
Ujjwal <gifah18076@reebsd.com>
2024-12-02 12:47:39 +0530

    Code commit successfully

---
+++ src/Demo/Main/Main.java
@@ -0,0 +1,15 @@
+package Demo.Main;
+
+public class Main {
+
+	public static void main(String ar[]) {
+		System.out.println(System.getProperty("user.dir")+System.getProperty("file.separator")+"datafiles"+System.getProperty("file.separator")+"data.xlsx");
+//		System.out.println(System.getProperty("file.separator"));
+//
+//		System.out.println("Hello world");
+//		for (int i = 1; i <= 10; i++) {
+//			System.out.println(i);
+//		}
+	}
+
+}
